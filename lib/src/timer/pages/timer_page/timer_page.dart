6666ac1c85c3babd7708a1138434bedf5d873cf5
Lucas Alves <l.alves001@gmail.com>
2022-08-03 11:11:24 -0300

    Setup timer file structure

---
+++ lib/src/timer/pages/timer_page/timer_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TimerPage extends StatefulWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  State<TimerPage> createState() => _TimerPageState();
+}
+
+class _TimerPageState extends State<TimerPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Timer"),
+      ),
+      body: const Center(),
+    );
+  }
+}
---
+++ lib/src/timer/pages/timer_page/widgets/buttons_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ButtonsContainer extends StatelessWidget {
+  const ButtonsContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
