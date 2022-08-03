daad01b26fc4a23484677abd01eb97ca1523685a
Lucas Alves <l.alves001@gmail.com>
2022-08-03 16:31:58 -0300

    Added Hive as another storage service implementation option

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'src/minimalist_app.dart';
 import 'src/timer/services/service_locator.dart';
 
-void main() {
-  setupGetIt();
+void main() async {
+  await setupGetIt();
   runApp(const MinimalistApp());
 }
--- lib/src/timer/services/service_locator.dart
+++ lib/src/timer/services/service_locator.dart
@@ -1,12 +1,19 @@
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import '../pages/timer_page/timer_page_controller.dart';
+import 'storage_service/hive_storage.dart';
 import 'storage_service/shared_preference_storage.dart';
 import 'storage_service/storage_service.dart';
 
 final getIt = GetIt.instance;
 
-void setupGetIt() {
+Future<void> setupGetIt({String storageService = 'hive'}) async {
   getIt.registerLazySingleton<TimerPageController>(() => TimerPageController());
-  getIt.registerLazySingleton<StorageService>(() => SharedPreferenceStorage());
+  if (storageService == 'hive') {
+    await Hive.initFlutter();
+    getIt.registerLazySingleton<StorageService>(() => HiveStorage());
+  } else {
+    getIt.registerLazySingleton<StorageService>(() => SharedPreferencesStorage());
+  }
 }
